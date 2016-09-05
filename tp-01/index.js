bec21209e8419c91e783e06f6ec1120a01eeec4e
sebassotom <sebassotom@hotmail.com>
2016-09-05 18:41:40 -0300

    Trabajo Practico 01

---
+++ tp-01/index.js
@@ -0,0 +1,24 @@
+var numeroA = prompt('Ingrese un número');
+var numeroB = prompt('Ingrese un número');
+
+var numA = parseInt (numeroA)
+var numB = parseInt (numeroB)
+
+	alert(numA + numB);
+
+
+var numeroC = prompt('Ingrese un número');
+var numeroD = prompt('Ingrese un número');
+
+	alert(numeroC - numeroD);
+
+var numeroE = prompt('Ingrese un número');
+var numeroF = prompt('Ingrese un número');
+
+	alert(numeroE * numeroF);
+
+
+var numeroG = prompt('Ingrese un número');
+var numeroH = prompt('Ingrese un número');
+
+	alert(numeroG / numeroH);
