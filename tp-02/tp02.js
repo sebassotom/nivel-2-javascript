e4cff432accf9f083705055a1e031edb6982174b
sebassotom <sebassotom@hotmail.com>
2016-09-12 18:40:31 -0300

    Commit tp2

--- tp-02/tp02.js
+++ tp-02/tp02.js
@@ -1,11 +1,33 @@
-var diaDeLaSemana = prompt('Ingrese un día de la semana');
-var diasDeLaSemana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+/* var finDeSemana = ['sabado', 'domingo'];
+var input = prompt('Ingrese un día de la semana');
+var semana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
+var mensaje = 'Usted ingresó un valor no válido.'
 
-for(position = 0; position < diasDeLaSemana.length; position++) {
-	if(diaDeLaSemana === diasDeLaSemana[position]) {
+for(i = 0; i < semana.length; i++) {
+	if(input === semana[i]) {
 		alert('Es un día hábil.');
+		break;
 	}
-	else {
+	
+	else if(i < finDeSemana.length && input === finDeSemana[i]) {
 		alert('Es un día de fin de semana.');
+		break;
+	}
+} */
+
+var input = prompt('Ingrese un día de la semana');
+var semana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+
+
+for(i = 0; i < semana.length; i++) {
+
+	if(input === semana[i] && i < 5) {
+		alert('Es un día hábil.');
+		break;
+	}
+
+	else if(input === semana[i] && i > 4) {
+		alert('Es un día de fin de semana.');
+		break;
 	}
 }
